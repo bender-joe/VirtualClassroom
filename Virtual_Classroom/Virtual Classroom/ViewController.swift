1984ca83e5faba41043065861bce0b3672d573d5
Joseph Bender <jbender94@knights.ucf.edu>
2015-10-18 13:56:43 -0400

    Revert "Updated LoginViewController"
    This reverts commit 94c489ca093a6b11042f18046ad39c342d704531.

--- Virtual_Classroom/Virtual Classroom/ViewController.swift
+++ Virtual_Classroom/Virtual Classroom/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-//    @IBAction func Login(sender: AnyObject) {
-//        activityIndicator = UIActivityIndicatorView( frame: CGRectMake(0, 0, 50, 50))
-//        activityIndicator.center = self.view.center
-//        activityIndicator.hidesWhenStopped = true
-//        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
-//        view.addSubview(activityIndicator)
-//        activityIndicator.startAnimating()
-//        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
-//    }
+    @IBAction func Login(sender: AnyObject) {
+        activityIndicator = UIActivityIndicatorView( frame: CGRectMake(0, 0, 50, 50))
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+    }
 
 }
 
