1de6167a72aca0f8067b8a462d0ffccd5e7b2bfa
Joseph Bender <jbender94@knights.ucf.edu>
2015-11-19 00:51:24 -0500

    Enrolled In finished... Users are now stored in Parse DB upon selecting course to enroll in

--- Virtual_Classroom/Virtual Classroom/SearchVC.swift
+++ Virtual_Classroom/Virtual Classroom/SearchVC.swift
@@ -17,11 +17,14 @@ class SearchVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var courseExamples = ["COP4710 Database Systems", "COP4331c Processes for OO", "EEL4742C Embedded Systems"]
     // Create the search results array
     var searchResults = [String]()
+    var courseIDs = [""]
     var checked = [Bool]()
+    var lastSelectedIndexPath : NSIndexPath?
     
     @IBAction func searchCourses(sender: AnyObject) {
         // get the strings of the user input
         searchResults.removeAll()
+        courseIDs.removeAll()
         
         // reset checks
 //        resetChecks()
@@ -33,21 +36,22 @@ class SearchVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             var query = PFQuery(className:"Course")
             query.whereKey("courseNumber", equalTo: number)
             query.findObjectsInBackgroundWithBlock {
-                (objects: [PFObject]?, error: NSError?) -> Void in
+                (objects, error) -> Void in
                 
                 if error == nil {
                     // The find succeeded.
                     print("Successfully retrieved \(objects!.count) scores.")
                     // Do something with the found objects
                     if let objects = objects as? [PFObject]! {
-                        for object in objects {
-                            var result = object["coursePrefix"] as! String
+                        for course in objects {
+                            var result = course["coursePrefix"] as! String
                             result += " "
-                            result.appendContentsOf(object["courseNumber"] as! String)
+                            result.appendContentsOf(course["courseNumber"] as! String)
                             result += " "
-                            result.appendContentsOf(object["courseName"] as! String)
+                            result.appendContentsOf(course["courseName"] as! String)
                             print(result)
                             self.searchResults.append(result)
+                            self.courseIDs.append(course.objectId!)
                             self.checked.append(false)
                             print(self.searchResults[0])
                         }
@@ -83,6 +87,7 @@ class SearchVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                             print(result)
                             self.searchResults.append(result)
                             self.checked.append(false)
+                            self.courseIDs.append(object.objectId!)
                             print(self.searchResults[0])
                         }
                     }
@@ -100,13 +105,52 @@ class SearchVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     // ENROLL BUTTON
     @IBAction func EnrollButton(sender: AnyObject) {
         // get the selected courses in the table
-        
-        // query and add them to the DB for the user
+        if(lastSelectedIndexPath != nil){
+            let selectedCourseID = courseIDs[(lastSelectedIndexPath?.row)!]
+            // query and add them to the DB for the user
+            let enrolling = PFObject(className: "Enrolled_In")
+            
+            print("enrolling "+(PFUser.currentUser()?.objectId)!+" "+selectedCourseID)
+            // before insert into enrolling check if student is enrolled
+            var query = PFQuery(className: "Enrolled_In")
+            query.whereKey("User", equalTo: PFUser.currentUser()!.objectId!)
+            query.whereKey("Course", equalTo: selectedCourseID)
+            
+            query.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
+                if let objects
+                // check enrolled_in and see if they are enrolled in the DB...
+            })
+            
+            
+            enrolling["User"] = PFUser.currentUser()
+            enrolling["Course"] = PFObject(withoutDataWithClassName: "Course", objectId: selectedCourseID)
+            enrolling.saveInBackground()
+//            performSegueWithIdentifier("done_enrolling", sender: nil)
+        }
         
     }
     
-    
-    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+            print(courseIDs[indexPath.row])
+        
+        // Code for checkmarks
+        if indexPath.row != lastSelectedIndexPath?.row {
+            if let lastSelectedIndexPath = lastSelectedIndexPath {
+                let oldCell = tableView.cellForRowAtIndexPath(lastSelectedIndexPath)
+                oldCell?.accessoryType = .None
+            }
+            
+            let newCell = tableView.cellForRowAtIndexPath(indexPath)
+            newCell?.accessoryType = .Checkmark
+            
+            lastSelectedIndexPath = indexPath
+        }
+//        let indexPath = resultsTable.indexPathForSelectedRow!
+//        
+//        let currentCell = resultsTable.cellForRowAtIndexPath(indexPath)
+//        
+//        print(currentCell!.textLabel!.text)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -141,22 +185,6 @@ class SearchVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             cell.accessoryType = .Checkmark
         }
         return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
-    {
-        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
-            if cell.accessoryType == .Checkmark
-            {
-                cell.accessoryType = .None
-                checked[indexPath.row] = false
-            }
-            else
-            {
-                cell.accessoryType = .Checkmark
-                checked[indexPath.row] = true
-            }
-        }    
     }
     
     override func didReceiveMemoryWarning() {
